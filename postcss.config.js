4f4e2899907f5d7437966c3128eac8ab2383180a
Mpass <121707487+ME942082@users.noreply.github.com>
2024-11-28 20:46:23 +0800

    recipe complate

---
+++ postcss.config.js
@@ -0,0 +1,47 @@
+
+module.exports = {
+    plugins: [
+        require('cssnano')({
+            preset: 'default',
+        }),
+        require('postcss-sorting')({
+            order: [
+                'custom-properties',
+                'dollar-variables',
+                'declarations',
+                'at-rules',
+                'rules',
+            ],
+
+            'properties-order': [
+                "display",
+                "position",
+                "float",
+                "clear",
+                "visibility",
+                "overflow",
+                "width",
+                "height",
+                "margin",
+                "padding",
+                "border",
+                "background",
+                "color",
+                "font",
+                "text-decoration",
+                "text-align",
+                "vertical-align",
+                "white-space",
+                "break-word",
+                "content",
+                "cursor",
+                "border-radius",
+                "box-shadow",
+                "text-shadow",
+                "background: linear-gradient"
+            ],
+
+            'unspecified-properties-position': 'bottom',
+        }),
+    ],
+};
